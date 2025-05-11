75f54ccd2046a10be97472bee88fa566e233de19
HIROYA1225 <area0466.d25@gmail.com>
2025-05-11 23:02:54 +0900

    ポリシーページ追加

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login_view, dashboard_view, success_view,landing_page_view,signup_login_view,index_1_view,confirm_signup_view,show_qr_view
+from .views import register, login_view, dashboard_view, success_view,landing_page_view,signup_login_view,index_1_view,confirm_signup_view,show_qr_view,privacy_policy_view
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', signup_login_view, name='signup_login'),
     path('confirm/', confirm_signup_view, name='confirm_signup'),
     path('entry/', show_qr_view, name='entry_qr'),
+    path('policy/', privacy_policy_view, name='policy'),
 ]
